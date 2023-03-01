6f50f1d844be9183d76f677d11b7cde6843160e3
Ahmed Elalamy <ahmedkamalelalamy@gmail.com>
2023-03-01 18:07:50 +0100

    Change Zoom + start investigating the Places API

--- app/src/main/java/ch/epfl/culturequest/MapsActivity.java
+++ app/src/main/java/ch/epfl/culturequest/MapsActivity.java
@@ -32,32 +32,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
+     * This is where we can add markers or lines, add listeners or move the camera.
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
+        // Add a marker at EPFL and move the camera
         LatLng EPFL = new LatLng(46.520536, 6.568318);
         LatLng satellite = new LatLng(46.520544, 6.567825);
         mMap.addMarker(new MarkerOptions().position(satellite).title("Satellite"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(EPFL));
-
         mMap.setOnInfoWindowClickListener(this);
+        // changes zoom
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(10f));
     }
 
     @Override
     public void onInfoWindowClick(Marker marker) {
-        Toast.makeText(this, marker.getPosition().toString() , Toast.LENGTH_LONG).show();
+        Toast.makeText(this, String.format("Lattitude: %f\nLongitude: %f", marker.getPosition().latitude, marker.getPosition().longitude) , Toast.LENGTH_LONG).show();
     }
 }
