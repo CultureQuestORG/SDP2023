628f90ca4108e8ecc59961334bce0d3a5c31b654
lucamouchel <luca.mouchel@epfl.ch>
2023-03-04 13:20:06 +0100

    edited google-services json to be used to sign in to app

---
+++ app/src/main/java/ch/epfl/culturequest/LoginActivity.java
@@ -0,0 +1,31 @@
+package ch.epfl.culturequest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+import androidx.activity.ComponentActivity;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+//import ch.epfl.culturequest.authentication.Authenticator;
+/**
+public class LoginActivity extends AppCompatActivity {
+    Authenticator auth = new Authenticator(this);
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        findViewById(R.id.sign_in_button).setOnClickListener(l ->{
+            auth.signIn();
+        });
+    }
+
+
+    public void logout(View v){
+        auth.signOut();
+    }
+}
+ */
