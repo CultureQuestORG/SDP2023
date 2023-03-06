980dd314a0d87cfcfca448ccf0dc4e458fa278a4
A <albert.troussard@epfl.ch>
2023-03-06 20:43:34 +0100

    fix: added comments on unclear method Database.init

--- app/src/main/java/ch/epfl/culturequest/database/Database.java
+++ app/src/main/java/ch/epfl/culturequest/database/Database.java
@@ -2,9 +2,16 @@ package ch.epfl.culturequest.database;
 
 import java.util.concurrent.CompletableFuture;
 
+
+
 public class Database  implements DatabaseInterface{
     private static DatabaseInterface  databaseInstance = new FireDatabase();
 
+    /**
+     * This method is used to initialize the database instance as something else than the default (FireDatabase)
+     * This is useful for testing purposes
+     * @param database the database instance to be used
+     */
     public static void init(DatabaseInterface database) {
         Database.databaseInstance = database;
     }
