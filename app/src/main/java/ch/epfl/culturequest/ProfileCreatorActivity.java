772743a1e70ae320c516f1f2b86524e656413988
lucamouchel <luca.mouchel@epfl.ch>
2023-03-07 15:34:59 +0100

    rolled back to only UI version of sign up. without any backend

--- app/src/main/java/ch/epfl/culturequest/ProfileCreatorActivity.java
+++ app/src/main/java/ch/epfl/culturequest/ProfileCreatorActivity.java
@@ -2,32 +2,33 @@ package ch.epfl.culturequest;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 import androidx.activity.ComponentActivity;
+import androidx.activity.result.ActivityResult;
 import androidx.annotation.Nullable;
-import androidx.annotation.XmlRes;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import ch.epfl.culturequest.database.Database;
+
 
 public class ProfileCreatorActivity extends ComponentActivity {
-    private final FirebaseUser USER = FirebaseAuth.getInstance().getCurrentUser();
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_creation);
-        assert USER != null;
-        ((TextView)findViewById(R.id.email_text)).setText(USER.getEmail());
-        String fullName = USER.getDisplayName();
-        String[] names = fullName.split(" ");
-        ((TextView)findViewById(R.id.first_name_text)).setText(names[0]);
-        ((TextView)findViewById(R.id.last_name_text)).setText(names[1]);
     }
 
 
-    public void createProfile(View view){
+    public void createProfile(View view) {
+        Database db = new Database();
+        //need albert here
+        //db.set(profile.getUid(), profile);
         System.out.println("created!!");
+    }
+
+    public void selectProfilePicture(View view) {
+    }
+
+    private void setProfilePicture(ActivityResult result) {
     }
 
 
