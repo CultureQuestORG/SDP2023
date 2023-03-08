66f166419275822738f76f87773ab4e51daa4aa4
lucamouchel <luca.mouchel@epfl.ch>
2023-03-08 16:25:50 +0100

    added username security checks and tests for profile activity

---
+++ app/src/androidTest/java/ch/epfl/culturequest/ProfileCreatorActivityTest.java
@@ -0,0 +1,73 @@
+package ch.epfl.culturequest;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.pressBack;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withHint;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThrows;
+
+import android.app.Instrumentation;
+import android.content.Intent;
+
+import androidx.test.espresso.PerformException;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class ProfileCreatorActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<ProfileCreatorActivity> testRule = new ActivityScenarioRule<>(ProfileCreatorActivity.class);
+
+    @Test
+    public void emailNotEditable(){
+        assertThrows(PerformException.class, () -> onView(withId(R.id.email_text)).perform(typeText("jane@gmail.com")));
+    }
+
+    @Test
+    public void correctUsernameTransitionsToNavActivity(){
+        Instrumentation.ActivityMonitor activityMonitor = getInstrumentation()
+                .addMonitor(NavigationActivity.class.getName(), null, false);
+
+        onView(withId(R.id.username)).perform(typeText("lucamouchel"));
+        onView(withId(R.id.create_profile)).perform(pressBack()).perform(click());
+
+
+        NavigationActivity secondActivity = (NavigationActivity) activityMonitor
+                .waitForActivityWithTimeout(5000);
+        assertNotNull(secondActivity);
+
+        Intent expectedIntent = new Intent(getInstrumentation().getTargetContext(), NavigationActivity.class);
+        assertEquals(expectedIntent.getComponent(), secondActivity.getIntent().getComponent());
+    }
+
+    @Test
+    public void wrongUserNameDoesntChangeIntent(){
+        Instrumentation.ActivityMonitor activityMonitor = getInstrumentation()
+                .addMonitor(NavigationActivity.class.getName(), null, false);
+        onView(withId(R.id.username)).perform(typeText("  !+ "));
+        onView(withId(R.id.create_profile)).perform(pressBack()).perform(click());
+        NavigationActivity secondActivity = (NavigationActivity) activityMonitor
+                .waitForActivityWithTimeout(2000);
+        assertNull(secondActivity);
+    }
+
+    @Test
+    public void wrongUserNameSetsHintText() throws InterruptedException {
+        onView(withId(R.id.username)).perform(typeText("  !+ "));
+        onView(withId(R.id.create_profile)).perform(pressBack()).perform(click());
+        Thread.sleep(2000);
+        onView(withId(R.id.username)).check(matches(withHint(ProfileCreatorActivity.INCORRECT_USERNAME_FORMAT)));
+    }
+}
--- app/src/main/java/ch/epfl/culturequest/ProfileCreatorActivity.java
+++ app/src/main/java/ch/epfl/culturequest/ProfileCreatorActivity.java
@@ -1,16 +1,23 @@
 package ch.epfl.culturequest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.ComponentActivity;
 import androidx.activity.result.ActivityResult;
 import androidx.annotation.Nullable;
 
+import java.util.Locale;
+
 import ch.epfl.culturequest.database.Database;
 
 
 public class ProfileCreatorActivity extends ComponentActivity {
+    public static String INCORRECT_USERNAME_FORMAT = "Incorrect Username Format";
+    public static String USERNAME_REGEX = "^[a-zA-Z0-9_-]+$";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +29,15 @@ public class ProfileCreatorActivity extends ComponentActivity {
         Database db = new Database();
         //need albert here
         //db.set(profile.getUid(), profile);
-        System.out.println("created!!");
+        EditText userName = findViewById(R.id.username);
+        if (usernameIsValid(userName.getText().toString())){
+            Intent successfulProfileCreation = new Intent(this, NavigationActivity.class);
+            startActivity(successfulProfileCreation);
+        }
+        else {
+            userName.setText("");
+            userName.setHint(INCORRECT_USERNAME_FORMAT);
+        }
     }
 
     public void selectProfilePicture(View view) {
@@ -32,4 +47,12 @@ public class ProfileCreatorActivity extends ComponentActivity {
     }
 
 
+    public boolean usernameIsValid(String username){
+        int length = username.length();
+        return !username.isEmpty()
+                && length > 3
+                && length < 20
+                && username.matches(USERNAME_REGEX)
+                && !username.contains(" ");
+    }
 }
