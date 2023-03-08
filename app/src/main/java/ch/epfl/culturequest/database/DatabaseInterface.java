4b35a86b3fe82c59ccefd815227cd173161e4bb2
A <albert.troussard@epfl.ch>
2023-03-08 10:38:03 +0100

    Created the Firebase Storage

--- app/src/main/java/ch/epfl/culturequest/database/DatabaseInterface.java
+++ app/src/main/java/ch/epfl/culturequest/database/DatabaseInterface.java
@@ -5,4 +5,7 @@ import java.util.concurrent.CompletableFuture;
 public interface DatabaseInterface {
     void set(String key, Object value);
     CompletableFuture<Object> get(String key);
+
+    CompletableFuture<Object> getUser(Object user);
+
 }
