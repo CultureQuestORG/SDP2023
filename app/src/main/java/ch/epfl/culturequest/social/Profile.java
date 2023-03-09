aef67eae9c2330452050ae5da6253d2ff840487a
lucamouchel <luca.mouchel@epfl.ch>
2023-03-09 13:45:43 +0100

    added doc

--- app/src/main/java/ch/epfl/culturequest/social/Profile.java
+++ app/src/main/java/ch/epfl/culturequest/social/Profile.java
@@ -15,6 +15,18 @@ public class Profile {
     private final String uid, name, username, email, phoneNumber;
     private Uri profilePicture;
 
+    /**
+     * Creates a Profile for a user.
+     *
+     * All Preconditions are checked in other classes. e.g., We create a profile
+     * only when the user has correctly signed in and has correctly set they're username.
+     * So user will normally never be null, and there is no need for sanitization of username as
+     * it's already been dealt with
+     *
+     * @param user current user signed in
+     * @param username username of user
+     * @param profilePicture Profile picture. Can be set to null
+     */
     public Profile(FirebaseUser user, String username, Uri profilePicture) {
         this.user = Objects.requireNonNull(user);
         this.username = username;
