3d70a90bed95453eb35dabe69858996912697339
TheGuyWithoutH <ugo.balducci@hotmail.fr>
2023-03-09 15:40:57 +0100

    feat: add CameraView and permissions

--- app/src/main/java/ch/epfl/culturequest/ui/scan/ScanFragment.java
+++ app/src/main/java/ch/epfl/culturequest/ui/scan/ScanFragment.java
@@ -1,22 +1,46 @@
 package ch.epfl.culturequest.ui.scan;
 
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.util.Size;
 import android.view.LayoutInflater;
+import android.view.Surface;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.google.android.material.snackbar.Snackbar;
+import java.util.List;
 
+import ch.epfl.culturequest.R;
 import ch.epfl.culturequest.databinding.FragmentScanBinding;
 
 public class ScanFragment extends Fragment {
 
     private FragmentScanBinding binding;
+    private HandlerThread backgroundThread;
+    private Handler backgroundHandler;
+    private CameraManager cameraManager;
+    private TextureView textureView;
+    private CameraCaptureSession cameraCaptureSession;
+    private CameraDevice cameraDevice;
+    private CaptureRequest captureRequest;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,6 +50,36 @@ public class ScanFragment extends Fragment {
         binding = FragmentScanBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        // Request the permissions
+        requestPermissions();
+
+        textureView = root.findViewById(R.id.camera_feedback);
+        cameraManager = (CameraManager) getActivity().getSystemService(getContext().CAMERA_SERVICE);
+        backgroundThread = new HandlerThread("Camera feedback");
+        backgroundThread.start();
+        backgroundHandler = new Handler(backgroundThread.getLooper());
+
+        textureView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
+            @Override
+            public void onSurfaceTextureAvailable(@NonNull android.graphics.SurfaceTexture surfaceTexture, int i, int i1) {
+                openCamera();
+            }
+            @Override
+            public void onSurfaceTextureSizeChanged(@NonNull SurfaceTexture surfaceTexture, int i, int i1) {
+
+            }
+
+            @Override
+            public boolean onSurfaceTextureDestroyed(@NonNull SurfaceTexture surfaceTexture) {
+                return false;
+            }
+
+            @Override
+            public void onSurfaceTextureUpdated(@NonNull SurfaceTexture surfaceTexture) {
+
+            }});
+
+
         // Adds a listener to the scan button and performs action
         binding.scanAction.scanButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,8 +88,6 @@ public class ScanFragment extends Fragment {
             }
         });
 
-        final TextView textView = binding.textScan;
-        ScanViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
@@ -43,5 +95,93 @@ public class ScanFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    // Permission is granted. Continue the action or workflow in your
+                    // app.
+                } else {
+                    // Explain to the user that the feature is unavailable because the
+                    // feature requires a permission that the user has denied. At the
+                    // same time, respect the user's decision. Don't link to system
+                    // settings in an effort to convince the user to change their
+                    // decision.
+                }
+            });
+
+    // Method to request the permissions
+    private void requestPermissions() {
+        if (ContextCompat.checkSelfPermission(
+                getContext(), android.Manifest.permission.CAMERA) ==
+                PackageManager.PERMISSION_GRANTED) {
+            // You can use the API that requires the permission.
+        } else {
+            // You can directly ask for the permission.
+            // The registered ActivityResultCallback gets the result of this request.
+            requestPermissionLauncher.launch(
+                    android.Manifest.permission.CAMERA);
+        }
+    }
+
+    private void openCamera() {
+        try {
+            cameraManager.openCamera("0", new CameraDevice.StateCallback() {
+                @Override
+                public void onOpened(@NonNull CameraDevice camera) {
+                    cameraDevice = camera;
+                    CaptureRequest.Builder builder = null;
+                    Size[] sizes;
+                    try {
+                         builder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+                         sizes = cameraManager.getCameraCharacteristics("0").get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP).getOutputSizes(SurfaceTexture.class);
+                    } catch (CameraAccessException e) {
+                        onError(camera, CameraDevice.StateCallback.ERROR_CAMERA_DEVICE);
+                        return;
+                    }
+
+                    textureView.getSurfaceTexture().setDefaultBufferSize(sizes[0].getWidth(), sizes[0].getHeight());
+                    Surface surfaceTexture = new Surface(textureView.getSurfaceTexture());
+                    builder.addTarget(surfaceTexture);
+
+                    CaptureRequest.Builder finalBuilder = builder;
+                    try {
+                        cameraDevice.createCaptureSession(List.of(surfaceTexture), new CameraCaptureSession.StateCallback() {
+                            @Override
+                            public void onConfigured(@NonNull CameraCaptureSession session) {
+                                cameraCaptureSession = session;
+                                try {
+                                    captureRequest = finalBuilder.build();
+                                    cameraCaptureSession.setRepeatingRequest(captureRequest, null, backgroundHandler);
+                                } catch (CameraAccessException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                            @Override
+                            public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+                                cameraCaptureSession = null;
+                            }
+                        }, backgroundHandler);
+                    } catch (CameraAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+                @Override
+                public void onDisconnected(@NonNull CameraDevice camera) {
+                    cameraDevice.close();
+                }
+
+                @Override
+                public void onError(@NonNull CameraDevice camera, int i) {
+                    cameraDevice.close();
+                    cameraDevice = null;
+                }
+            }, backgroundHandler);
+        } catch (SecurityException | CameraAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
