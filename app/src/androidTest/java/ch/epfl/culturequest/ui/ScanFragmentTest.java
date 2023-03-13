9c26fc245f16a5e75a6476c9301c1ed521cac9b1
Hugo MIRANDA QUEIROS <78116627+hmiranda-queiros@users.noreply.github.com>
2023-03-13 13:23:25 +0100

    feat: Add backend to save images locally (#31)
    * Add the backend to store images locally when a picture is taken with the camera by clicking on the scan button.
    
    * If the Wifi is available then the picture will be stored normally, otherwise it is stored with the prefix "pending_" in order to highlight that it still needs to be sent to Google Lens as soon as the Wifi is available again.
    
    * All the pictures are stored in the primary shared storage in order to have automatic READ/WRITE access and to be able to share the picture in other app in the future. This also prevents users from loosing all their pictures if they uninstall the app.
    
    * Add all the tests to verify that the correct image is stored depending on the Wifi status.
    
    * Gradle updated to 4.7.2

--- app/src/androidTest/java/ch/epfl/culturequest/ui/ScanFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/culturequest/ui/ScanFragmentTest.java
@@ -1,14 +1,24 @@
 package ch.epfl.culturequest.ui;
 
+import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.hamcrest.Matchers.is;
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.android21buttons.fragmenttestrule.FragmentTestRule;
 
+import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,5 +34,38 @@ public class ScanFragmentTest {
     @Test
     public void textViewDisplaysCorrectText() {
         onView(withId(R.id.text_scan)).check(matches(withText("This is scan fragment")));
+    }
+
+    @Test
+    public void clickOnScanButtonStoresOneImageInSharedStorage() {
+        int initialCount = countImagesInSharedStorage();
+        onView(withId(R.id.scan_button)).perform(click());
+        int finalCount = countImagesInSharedStorage();
+        assertThat(finalCount, is(initialCount + 1));
+    }
+
+    @Test
+    @After
+    public void deleteAllImagesInSharedStorage() {
+        Uri collection = fragmentTestRule.getFragment().localStorage.contentUri;
+
+        ContentResolver contentResolver = getApplicationContext().getContentResolver();
+        contentResolver.delete(collection, null, null);
+        int totalCount = countImagesInSharedStorage();
+        assertThat(totalCount, is(0));
+    }
+
+    private int countImagesInSharedStorage() {
+        Uri collection = fragmentTestRule.getFragment().localStorage.contentUri;
+
+        // Counts the number of ready images (not pending) in the shared storage
+        try (Cursor cursor = getApplicationContext().getContentResolver().query(
+                collection,
+                null,
+                null,
+                null,
+                null)) {
+            return cursor.getCount();
+        }
     }
 }
