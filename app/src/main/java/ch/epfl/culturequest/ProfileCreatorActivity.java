14a060ee7cbd87487721e1c78f6c908837c9ac64
luca mouchel <73081373+lucamouchel@users.noreply.github.com>
2023-03-17 00:25:18 +0100

    Feat: Sign in page (#25)
    * working version of the FirebaseActivity
    
    * authentication service for google with added dependencies for gradle
    
    * reset signin result to redirect to main activity & removed json file to prevent merge conflicts with other branch
    
    * Created a Database interface with 2 implementations:
    -FireDatabase
    -MockDatabase
    
    Tested the activity
    
    * updated the test
    
    * fixed the xml of the firebase Activity
    
    * changed the design of the firebase Activity
    
    * add test to goo to the activity
    
    * renamed tests
    
    * added redirection methods to redirect to sign in page. added getter for user
    
    * rolled back to version 7.4.1
    
    * feat: design of sign in page + sign in activity
    
    * added profile creation logic and page with several XML files to style textbox and buttons
    
    * feat: created User and Profile class and implemented profile picture feature
    
    * rolled back to only UI version of sign up. without any backend
    
    * resolved gradle failed errors
    
    * reset main activity to before and rolled back version
    
    * modified UI to ask for username instead of first and last name
    
    * added username security checks and tests for profile activity
    
    * removeed picasso dependancy (might be the problem for error when CI is testing)
    
    * added simple test for signup activity
    
    * modified authenticator with bool attribute for testing purposes + added tests for authentication
    
    * refactord Authenticator.java
    
    * refactored redirection method
    
    * removed email field in profile creation xml
    
    * rolled back version and removed a test
    
    * fixed issues with testing where build failed on the CI
    
    * refactored Profile class, and added tests for profile creation activity
    
    * repositioned login button and removed a test
    
    * refactored test to delete user after running tests
    
    * added tests for circle transform
    
    * adjusted test and fixed build.gradle
    
    * fixed tests that added anonymous users to db
    
    * attempt to fixe codeclimate issue
    
    ---------
    
    Co-authored-by: A <albert.troussard@epfl.ch>
    Co-authored-by: A <75203799+alberttkt@users.noreply.github.com>

---
+++ app/src/main/java/ch/epfl/culturequest/ProfileCreatorActivity.java
@@ -0,0 +1,147 @@
+package ch.epfl.culturequest;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.squareup.picasso.Picasso;
+
+import java.util.Objects;
+
+import ch.epfl.culturequest.database.Database;
+import ch.epfl.culturequest.social.Profile;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+
+public class ProfileCreatorActivity extends AppCompatActivity {
+    public static String INCORRECT_USERNAME_FORMAT = "Incorrect Username Format";
+    public static String USERNAME_REGEX = "^[a-zA-Z0-9_-]+$";
+    public static String DEFAULT_PROFILE_PATH = "res/drawable/profile_icon_selector.xml";
+
+    private final String GALLERY_PERMISSION = Manifest.permission.READ_EXTERNAL_STORAGE;
+    private final Profile profile = new Profile(null, null);
+    private final ActivityResultLauncher<Intent> profilePictureSelector = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(), this::displayProfilePic);
+    private final ActivityResultLauncher<String> requestPermissionLauncher =
+            this.registerForActivityResult(new ActivityResultContracts.RequestPermission(),
+                    isGranted -> {
+                        if (isGranted) openGallery();
+                    });
+    private ImageView profileView;
+    private Drawable initialDrawable;
+    private Database db = new Database();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_creation);
+        //the following attributes are used to check whether the user actually selected a profile pic
+        profileView = findViewById(R.id.profile_picture);
+        initialDrawable = profileView.getDrawable();
+    }
+
+    @Override
+    public void onBackPressed() {
+        //do nothing!! We don't want the user to go back to the sign in page
+    }
+
+    /**
+     * Called when clicking on the add profile pic icon. Basically asks for permissions
+     * to read external storage, then opens the gallery for the user to select a profile pic
+     *
+     * @param view
+     */
+    public void selectProfilePicture(View view) {
+        if (ContextCompat.checkSelfPermission(this, GALLERY_PERMISSION) == PackageManager.PERMISSION_GRANTED) {
+            openGallery();
+        } else {
+            requestPermissionLauncher.launch(GALLERY_PERMISSION);
+        }
+    }
+
+    /**
+     * Function called when user clicks on the buttont "Create my Account"
+     * First checks if username is valid and if user has selected a profile pic,
+     * then registers the Profile in the Database and redirects to the Navigation Intent
+     * TODO need to store the profile in the Database
+     *
+     * @param view
+     */
+    public void createProfile(View view) {
+        EditText textView = findViewById(R.id.username);
+        String username = textView.getText().toString();
+        if (isValid(username)) {
+            setDefaultPicIfNoneSelected();
+            if (!Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).isAnonymous()) {
+                db.setProfile(profile);
+            }
+            Intent successfulProfileCreation = new Intent(this, NavigationActivity.class);
+            startActivity(successfulProfileCreation);
+        } else {
+            textView.setText("");
+            textView.setHint(INCORRECT_USERNAME_FORMAT);
+        }
+    }
+
+    private void openGallery() {
+        profilePictureSelector.launch(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI));
+    }
+
+    private boolean isValid(String username) {
+        if (usernameIsValid(username)) {
+            profile.setUsername(username);
+            return true;
+        }
+        return false;
+    }
+
+    private void setDefaultPicIfNoneSelected() {
+        if (profileView.getDrawable().equals(initialDrawable)) {
+            profile.setProfilePicture(DEFAULT_PROFILE_PATH);
+        }
+    }
+
+
+    private void displayProfilePic(ActivityResult result) {
+        if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+            Uri profilePicture = result.getData().getData();
+            CircleImageView image = findViewById(R.id.profile_picture);
+            Picasso.get().load(profilePicture).into(image);
+            ((TextView) findViewById(R.id.profile_pic_text)).setText("");
+            profile.setProfilePicture(profilePicture.getPath());
+        }
+    }
+
+    private boolean usernameIsValid(String username) {
+        int length = username.length();
+        return !username.isEmpty()
+                && length > 3
+                && length < 20
+                && username.matches(USERNAME_REGEX)
+                && !username.contains(" ");
+    }
+
+    //used for testing purposes
+
+
+    public Profile getProfile() {
+        return profile;
+    }
+
+}
