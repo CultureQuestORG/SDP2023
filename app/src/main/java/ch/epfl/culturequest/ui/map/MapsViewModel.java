c12c129d5c35fca29132ea64028286d8c0cfa0e9
Ahmed Elalamy <ahmedkamalelalamy@gmail.com>
2023-03-16 17:35:28 +0100

    Add documentation

--- app/src/main/java/ch/epfl/culturequest/ui/map/MapsViewModel.java
+++ app/src/main/java/ch/epfl/culturequest/ui/map/MapsViewModel.java
@@ -15,28 +15,48 @@ public class MapsViewModel extends ViewModel{
 
     private final MutableLiveData<LatLng> currentLocation;
 
+    /**
+     * Constructor for the MapsViewModel
+     */
     public MapsViewModel() {
         isLocationPermissionGranted = new MutableLiveData<>(false);
         currentLocation = new MutableLiveData<>(DEFAULT);
     }
 
+    /**
+     * Method to check if the location permission is granted
+     * @return true if the location permission is granted, false otherwise
+     */
     public boolean isLocationPermissionGranted() {
         return Boolean.TRUE.equals(isLocationPermissionGranted.getValue());
     }
 
+    /**
+     * Method to set the location permission
+     * @param permission the permission to set
+     */
     public void setIsLocationPermissionGranted(boolean permission){
         isLocationPermissionGranted.setValue(permission);
     }
-
+    /**
+     * Method to get the current location
+     * @return the current location
+     */
     public MutableLiveData<LatLng> getCurrentLocation() {
         return currentLocation;
     }
-
+    /**
+     * Method to set the current location
+     * @param current the current location to set
+     */
     public void setCurrentLocation(LatLng current){
         Objects.requireNonNull(current, "Current location cannot be null");
         currentLocation.setValue(current);
     }
 
+    /**
+     * Method to reset the current location to the default location
+     */
     public void resetCurrentLocation(){
         currentLocation.setValue(DEFAULT);
     }
