3d1690f43b63b50db7c45eed868cc543b3dc5dfa
Ahmed Elalamy <ahmedkamalelalamy@gmail.com>
2023-03-20 14:10:14 +0100

    Revert "Remove all bootcamp activities and associated code"
    This reverts commit efcb66208eb119f6cab06a1dbbc206ebac5e7bf5.

---
+++ app/src/androidTest/java/ch/epfl/culturequest/MainActivityTest.java
@@ -0,0 +1,41 @@
+package ch.epfl.culturequest;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.matcher.IntentMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> testRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void clickOnNavButtonFiresIntentOfNavigationActivity() {
+        Intents.init();
+        onView(withId(R.id.navButton)).perform(click());
+        intended(hasComponent(NavigationActivity.class.getName()));
+        Intents.release();
+    }
+
+    @Test
+    public void ClickOnFirebaseButtonFiresFirebaseActivity() {
+        onView(withId(R.id.firebaseButton)).perform(click());
+        //check if we are in the firebase activity
+        onView(withId(R.id.editTextPhone)).check(matches(withText("")));
+        onView(withId(R.id.editTextEmailAddress)).check(matches(withText("")));
+    }
+
+}
