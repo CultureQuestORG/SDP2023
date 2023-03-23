9d65ff5f6399fd6a8d1431846f5c683e65b188e9
lucamouchel <luca.mouchel@epfl.ch>
2023-03-23 15:56:53 +0100

    added navigation to fragments

--- app/src/main/java/ch/epfl/culturequest/utils/Redirect.java
+++ app/src/main/java/ch/epfl/culturequest/utils/Redirect.java
@@ -3,16 +3,22 @@ package ch.epfl.culturequest.utils;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.view.View;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.Navigation;
 
 import ch.epfl.culturequest.R;
 
 public class Redirect {
     public static void toActivity(Activity src, Class<? extends Activity> dest){
         src.startActivity(new Intent(src, dest));
+    }
+
+    public static void toFragment(View view, int id){
+        Navigation.findNavController(view).navigate(id);
     }
 }
