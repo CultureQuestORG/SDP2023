8c50371fae934e6c54cb2637acfb7451a0f70e68
A <albert.troussard@epfl.ch>
2023-03-24 10:58:58 +0100

    fixed the SignUpActivity

--- app/src/main/java/ch/epfl/culturequest/SignUpActivity.java
+++ app/src/main/java/ch/epfl/culturequest/SignUpActivity.java
@@ -9,18 +9,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.concurrent.CompletableFuture;
+
 import ch.epfl.culturequest.authentication.Authenticator;
+import ch.epfl.culturequest.database.Database;
 import ch.epfl.culturequest.social.Profile;
 
 public class SignUpActivity extends AppCompatActivity {
     private final Authenticator auth = new Authenticator(this, false);
-    private final Profile activeProfile = Profile.getActiveProfile();
+    private final FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (activeProfile == null) {
+        if (currentUser == null) {
             setContentView(R.layout.activity_signin);
             findViewById(R.id.sign_in_button).setOnClickListener(k -> auth.signIn());
-        } else startActivity(new Intent(this, NavigationActivity.class));
+        } else {
+            if (Profile.getActiveProfile() == null) {
+                CompletableFuture<Profile> profile = Database.getProfile(currentUser.getUid());
+                profile.thenAccept(p -> {
+                    if (p == null) {
+                        startActivity(new Intent(this, ProfileCreatorActivity.class));
+                    } else {
+                        Profile.setActiveProfile(p);
+                        startActivity(new Intent(this, NavigationActivity.class));
+                    }
+                });
+            } else {
+                startActivity(new Intent(this, NavigationActivity.class));
+            }
+        }
     }
 }
