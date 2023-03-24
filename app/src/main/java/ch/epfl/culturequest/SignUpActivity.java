b7af03a1f1d77f222754866c4b93a140eb5edd8e
A <albert.troussard@epfl.ch>
2023-03-24 11:14:38 +0100

    simplified signUp

--- app/src/main/java/ch/epfl/culturequest/SignUpActivity.java
+++ app/src/main/java/ch/epfl/culturequest/SignUpActivity.java
@@ -18,26 +18,37 @@ import ch.epfl.culturequest.social.Profile;
 public class SignUpActivity extends AppCompatActivity {
     private final Authenticator auth = new Authenticator(this, false);
     private final FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // If the user is not logged in, display the sign in activity
         if (currentUser == null) {
             setContentView(R.layout.activity_signin);
             findViewById(R.id.sign_in_button).setOnClickListener(k -> auth.signIn());
-        } else {
-            if (Profile.getActiveProfile() == null) {
-                CompletableFuture<Profile> profile = Database.getProfile(currentUser.getUid());
-                profile.thenAccept(p -> {
-                    if (p == null) {
-                        startActivity(new Intent(this, ProfileCreatorActivity.class));
-                    } else {
-                        Profile.setActiveProfile(p);
-                        startActivity(new Intent(this, NavigationActivity.class));
-                    }
-                });
-            } else {
-                startActivity(new Intent(this, NavigationActivity.class));
-            }
+            return;
         }
+        // If the user is logged in, check if he has a profile
+        if (Profile.getActiveProfile() == null) {
+            CompletableFuture<Profile> profile = Database.getProfile(currentUser.getUid());
+            profile.handle((p, e) -> {
+                if (e != null) {
+                    // If the user does not have a profile, display the profile creator activity
+                    startActivity(new Intent(this, ProfileCreatorActivity.class));
+                } else {
+                    // If the user has a profile, display the navigation activity
+                    Profile.setActiveProfile(p);
+                    startActivity(new Intent(this, NavigationActivity.class));
+                }
+                return null;
+            }
+
+            );
+            return;
+        }
+
+        // If the user is logged in and has a profile, display the navigation activity
+        startActivity(new Intent(this, NavigationActivity.class));
+
     }
 }
