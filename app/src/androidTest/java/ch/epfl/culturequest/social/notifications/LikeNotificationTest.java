e4fec62f073d41f3dbaf567910953a888c0d3078
Ahmed Elalamy <91589855+aelalamy42@users.noreply.github.com>
2023-04-20 10:16:49 +0200

    Create basic notification infrastructure (#95)
    * Add basic notification implementations
    
    * Finish basic notifications implementations and testing
    
    * optimize imports
    
    * Solve code climate issues
    
    * Merge main
    
    * Revise code after merging

---
+++ app/src/androidTest/java/ch/epfl/culturequest/social/notifications/LikeNotificationTest.java
@@ -0,0 +1,38 @@
+package ch.epfl.culturequest.social.notifications;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import android.app.Notification;
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ch.epfl.culturequest.R;
+import ch.epfl.culturequest.social.Profile;
+
+
+public class LikeNotificationTest {
+
+    // Tests that the like notification is correctly created
+    @Test
+    public void testLikeNotification() {
+        Profile.setActiveProfile(new Profile("test", "Likee", "test", "test", "test", "test", List.of(), new ArrayList<>(),0));
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        android.app.Notification likeNotification = new LikeNotification("Liker").get(context);
+        assertThat(likeNotification.extras.get(Notification.EXTRA_TITLE).toString(), is("Likee, you have a new like!"));
+        assertThat(likeNotification.extras.get(Notification.EXTRA_TEXT).toString(), is("Liker liked your post!"));
+        assertThat(likeNotification.priority, is(Notification.PRIORITY_DEFAULT));
+        assertThat(likeNotification.getSmallIcon().getResId(), is(R.drawable.logo_compact));
+        assertThat(likeNotification.getChannelId(), is(context.getString(R.string.likeNotifChannelID)));
+
+        Profile.setActiveProfile(null);
+    }
+
+}
