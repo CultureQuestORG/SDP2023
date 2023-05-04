396d6029972efd3b8dfa0a8b5cee24e2a4136e90
Thomas Houhou <52637916+Th0h0@users.noreply.github.com>
2023-05-04 14:08:55 +0200

    Error message for art processing failure (#114)
    * Display art processing errors
    
    - Better handle future exceptions
    - Create new exceptions dedicated to the different stages
    - Display error message depending on the triggered exception associated to the failing service

---
+++ app/src/main/java/ch/epfl/culturequest/backend/exceptions/RecognitionFailedException.java
@@ -0,0 +1,8 @@
+package ch.epfl.culturequest.backend.exceptions;
+
+public class RecognitionFailedException extends Exception {
+    public RecognitionFailedException(String message) {
+        super(message);
+    }
+}
+
