54a297e75de8efa51d6c2a700f6b80fd6e4cd1bf
lucamouchel <luca.mouchel@epfl.ch>
2023-05-04 09:12:38 +0200

    added a test for the popup error msg

--- app/src/androidTest/java/ch/epfl/culturequest/util/UtilsTest.java
+++ app/src/androidTest/java/ch/epfl/culturequest/util/UtilsTest.java
@@ -1,9 +1,11 @@
 package ch.epfl.culturequest.util;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import androidx.activity.ComponentActivity;
 import androidx.test.core.app.ActivityScenario;
@@ -16,10 +18,11 @@ import org.junit.runner.RunWith;
 import ch.epfl.culturequest.NavigationActivity;
 import ch.epfl.culturequest.R;
 import ch.epfl.culturequest.SignUpActivity;
+import ch.epfl.culturequest.authentication.Authenticator;
 import ch.epfl.culturequest.utils.AndroidUtils;
 
 @RunWith(AndroidJUnit4.class)
-public class RedirectTest {
+public class UtilsTest {
     private ComponentActivity activity;
 
     @Before
@@ -34,5 +37,16 @@ public class RedirectTest {
     public void redirectingToActivityChangesCurrActivity() {
         AndroidUtils.redirectToActivity(activity, NavigationActivity.class);
         onView(withId(R.id.navigation_scan)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void popupShowsCorrectly() throws InterruptedException {
+        //doesnt work to directly run the popup function
+        activity.runOnUiThread(()->{
+            AndroidUtils.showNoConnectionAlert(activity, "TEST");
+        });
+        Thread.sleep(2000);
+        onView(withText("TEST")).check(matches(isDisplayed()));
+        onView(withText("OK")).perform(click());
     }
 }
