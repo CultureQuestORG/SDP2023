d4144a17e4e61b7ba3430dc66dd29a0367d3ed4a
lucamouchel <luca.mouchel@epfl.ch>
2023-05-04 09:38:26 +0200

    rolled back test

--- app/src/androidTest/java/ch/epfl/culturequest/util/RedirectTest.java
+++ app/src/androidTest/java/ch/epfl/culturequest/util/RedirectTest.java
@@ -1,11 +1,9 @@
 package ch.epfl.culturequest.util;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import androidx.activity.ComponentActivity;
 import androidx.test.core.app.ActivityScenario;
@@ -18,11 +16,10 @@ import org.junit.runner.RunWith;
 import ch.epfl.culturequest.NavigationActivity;
 import ch.epfl.culturequest.R;
 import ch.epfl.culturequest.SignUpActivity;
-import ch.epfl.culturequest.authentication.Authenticator;
 import ch.epfl.culturequest.utils.AndroidUtils;
 
 @RunWith(AndroidJUnit4.class)
-public class UtilsTest {
+public class RedirectTest {
     private ComponentActivity activity;
 
     @Before
@@ -37,16 +34,5 @@ public class UtilsTest {
     public void redirectingToActivityChangesCurrActivity() {
         AndroidUtils.redirectToActivity(activity, NavigationActivity.class);
         onView(withId(R.id.navigation_scan)).check(matches(isDisplayed()));
-    }
-
-    @Test
-    public void popupShowsCorrectly() throws InterruptedException {
-        //doesnt work to directly run the popup function
-        activity.runOnUiThread(()->{
-            AndroidUtils.showNoConnectionAlert(activity, "TEST");
-        });
-        Thread.sleep(3000);
-        onView(withText("TEST")).check(matches(isDisplayed()));
-        onView(withText("OK")).perform(click());
     }
 }
