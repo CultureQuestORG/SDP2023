e4bdb26374a637a62258081ac43ac5e2a04e5e44
lucamouchel <luca.mouchel@epfl.ch>
2023-05-10 14:48:43 +0200

    fixed codeclimate issues

--- app/src/main/java/ch/epfl/culturequest/ui/SightseeingActivity.java
+++ app/src/main/java/ch/epfl/culturequest/ui/SightseeingActivity.java
@@ -1,28 +1,16 @@
 package ch.epfl.culturequest.ui;
 
-import static androidx.test.InstrumentationRegistry.getContext;
-
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.util.Log;
-import android.util.MutableInt;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
@@ -30,11 +18,8 @@ import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
@@ -43,11 +28,13 @@ import ch.epfl.culturequest.backend.map_collection.OTMLatLng;
 import ch.epfl.culturequest.backend.map_collection.OTMLocation;
 import ch.epfl.culturequest.backend.map_collection.OTMLocationSerializer;
 import ch.epfl.culturequest.database.Database;
+import ch.epfl.culturequest.databinding.SightseeingActivityBinding;
 import ch.epfl.culturequest.social.Profile;
 import ch.epfl.culturequest.utils.SightSeeingArrayAdapter;
 
 public class SightseeingActivity extends AppCompatActivity {
 
+    private SightseeingActivityBinding binding;
     private ImageView backButton;
     private Button inviteFriends, preview;
     private View mapFragment;
@@ -59,39 +46,25 @@ public class SightseeingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sightseeing_activity);
-        listView = findViewById(R.id.places_to_see);
+        binding = SightseeingActivityBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        listView = binding.placesToSee;
         String city = getIntent().getStringExtra("city");
-        ((TextView) findViewById(R.id.city_text)).setText("What to do in " + city.split(",")[0] + "?");
-        inviteFriends = findViewById(R.id.invite_friends);
-        preview = findViewById(R.id.preview);
-        backButton = findViewById(R.id.back_button);
-        mapFragment = findViewById(R.id.map_fragment);
+        binding.cityText.setText("What to do in " + city.split(",")[0] + "?");
+        inviteFriends = binding.inviteFriends;
+        preview = binding.preview;
+        backButton = binding.backButton;
+        mapFragment = binding.mapFragment;
         backButton.setOnClickListener(l -> onBackPressed());
-        Map<String, OTMLocation> placeToLocation = getIntent().getStringArrayListExtra("locations").stream().map(OTMLocationSerializer::deserialize).collect(Collectors.toMap(
-                OTMLocation::getName,
-                location -> location,
-                (existing, replacement) -> existing));
+        Map<String, OTMLocation> placeToLocation = getIntent().getStringArrayListExtra("locations").stream().map(OTMLocationSerializer::deserialize).collect(Collectors.toMap(OTMLocation::getName, location -> location));
         adapter = new SightSeeingArrayAdapter(this, android.R.layout.simple_list_item_1, new ArrayList<>(placeToLocation.keySet()), List.of(preview, inviteFriends));
         listView.setAdapter(adapter);
         List<String> selected = adapter.getSelectedPlaces();
-
         preview.setOnClickListener(l -> {
             inviteFriends.setVisibility(View.INVISIBLE);
             openMap(placeToLocation, selected);
         });
-
-        inviteFriends.setOnClickListener(l -> {
-            Profile.getActiveProfile().retrieveFriends()
-                    .thenApply(friends -> friends.stream().map(Database::getProfile).collect(Collectors.toList()))
-                    .thenCompose(futures -> CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
-                            .thenApply(v -> futures.stream().map(CompletableFuture::join).collect(Collectors.toList())))
-                    .whenComplete((profiles, t) -> {
-                inviteFriends.setText("Send Invite");
-                SightSeeingArrayAdapter arrayAdapter = new SightSeeingArrayAdapter(this, android.R.layout.simple_list_item_1, profiles.stream().map(Profile::getUsername).collect(Collectors.toList()), List.of(inviteFriends));
-                listView.setAdapter(arrayAdapter);
-            });
-        });
+        inviteFriends.setOnClickListener(l -> handleFriendsLogic());
     }
 
     /**
@@ -112,6 +85,18 @@ public class SightseeingActivity extends AppCompatActivity {
         }
     }
 
+    private void handleFriendsLogic(){
+        Profile.getActiveProfile().retrieveFriends()
+                .thenApply(friends -> friends.stream().map(Database::getProfile).collect(Collectors.toList()))
+                .thenCompose(futures -> CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
+                        .thenApply(v -> futures.stream().map(CompletableFuture::join).collect(Collectors.toList())))
+                .whenComplete((profiles, t) -> {
+                    inviteFriends.setText("Send Invite");
+                    SightSeeingArrayAdapter arrayAdapter = new SightSeeingArrayAdapter(this, android.R.layout.simple_list_item_1, profiles.stream().map(Profile::getUsername).collect(Collectors.toList()), List.of(inviteFriends));
+                    listView.setAdapter(arrayAdapter);
+                });
+    }
+
     /**
      * This function will open a google map fragment, displaying all the selected places a user
      * chose to visit
@@ -119,15 +104,10 @@ public class SightseeingActivity extends AppCompatActivity {
      * @param placeToLocation map of places' names and their respective OTMLocation
      * @param selected        the list of selected place's name
      */
-    public void openMap(Map<String, OTMLocation> placeToLocation, List<String> selected) {
-        Map<String, OTMLocation> selectedPlaces = placeToLocation.entrySet()
-                .stream()
-                .filter(entry -> selected.contains(entry.getKey()))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    private void openMap(Map<String, OTMLocation> placeToLocation, List<String> selected) {
+        Map<String, OTMLocation> selectedPlaces = placeToLocation.entrySet().stream().filter(entry -> selected.contains(entry.getKey())).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
         mapFragment.setVisibility(View.VISIBLE);
-        backButton.setOnClickListener(l2 -> {
-            onBackPressed();
-        });
+        backButton.setOnClickListener(l2 -> onBackPressed());
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
         mapFragment.getMapAsync(googleMap -> {
             googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.maps_style_json));
@@ -138,9 +118,7 @@ public class SightseeingActivity extends AppCompatActivity {
             for (OTMLocation location : selectedPlaces.values()) {
                 OTMLatLng coord = location.getCoordinates();
                 LatLng mapCoord = new LatLng(coord.latitude(), coord.longitude());
-                MarkerOptions markerOptions = new MarkerOptions()
-                        .position(mapCoord)
-                        .title(location.getName());
+                MarkerOptions markerOptions = new MarkerOptions().position(mapCoord).title(location.getName());
                 googleMap.addMarker(markerOptions);
             }
             LatLngBounds.Builder builder = new LatLngBounds.Builder();
@@ -152,5 +130,4 @@ public class SightseeingActivity extends AppCompatActivity {
             googleMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 100));
         });
     }
-
 }
