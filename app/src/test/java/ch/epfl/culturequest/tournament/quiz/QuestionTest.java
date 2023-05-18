7a14844796eea360c4916a4cd0fe7acebb89b917
A <albert.troussard@epfl.ch>
2023-05-18 12:23:10 +0200

    add tests

---
+++ app/src/test/java/ch/epfl/culturequest/tournament/quiz/QuestionTest.java
@@ -0,0 +1,70 @@
+package ch.epfl.culturequest.tournament.quiz;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class QuestionTest{
+
+    Question question;
+
+    @Before
+    public void setUp(){
+        ArrayList<String> answers = new ArrayList<>();
+        answers.add("answer");
+        answers.add("wrongAnswer1");
+        answers.add("wrongAnswer2");
+        answers.add("wrongAnswer3");
+        question = new Question("question", answers,0);
+    }
+
+    @Test
+    public void setAndGetQuestion(){
+        question.setQuestion("newQuestion");
+        assertEquals("newQuestion", question.getQuestion());
+        question.setQuestion("question");
+    }
+
+    @Test
+    public void setAndGetPossibilities(){
+        ArrayList<String> answers = new ArrayList<>();
+        answers.add("answer");
+        answers.add("WrongAnswer1");
+        answers.add("WrongAnswer2");
+        answers.add("WrongAnswer3");
+        question.setPossibilities(answers);
+        assertEquals(answers, question.getPossibilities());
+    }
+
+    @Test
+    public void setAndGetCorrectAnswer(){
+        question.setAnswer(1);
+        assertEquals(1, question.getAnswer());
+        question.setAnswer(0);
+    }
+
+    @Test
+    public void constructor(){
+        ArrayList<String> answers = new ArrayList<>();
+        answers.add("answer");
+        answers.add("wrongAnswer1");
+        answers.add("wrongAnswer2");
+        answers.add("wrongAnswer3");
+        Question question = new Question("question", answers,0);
+        assertEquals("question", question.getQuestion());
+        assertEquals(answers, question.getPossibilities());
+        assertEquals(0, question.getAnswer());
+    }
+
+    @Test
+    public void isCorrect(){
+        assertTrue(question.isCorrect(0));
+        assertFalse(question.isCorrect(1));
+    }
+
+}
---
+++ app/src/test/java/ch/epfl/culturequest/tournament/quiz/QuizTest.java
@@ -0,0 +1,59 @@
+package ch.epfl.culturequest.tournament.quiz;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class QuizTest {
+
+    Quiz quiz;
+
+    @Before
+    public void setUp() {
+        quiz = new Quiz("artName", null, "tournament");
+    }
+
+
+    @Test
+    public void setAndGetArtName() {
+        quiz.setArtName("newArtName");
+        assertEquals("newArtName", quiz.getArtName());
+        quiz.setArtName("artName");
+    }
+
+    @Test
+    public void setAndGetQuestions() {
+        ArrayList<String> answers = new ArrayList<>();
+        answers.add("answer");
+        answers.add("wrongAnswer1");
+        answers.add("wrongAnswer2");
+        answers.add("wrongAnswer3");
+        Question question = new Question("question", answers,0);
+        ArrayList<Question> questions = new ArrayList<>();
+        questions.add(question);
+        quiz.setQuestions(questions);
+        assertEquals(questions, quiz.getQuestions());
+    }
+
+    @Test
+    public void setAndGetTournament() {
+        quiz.setTournament("newTournament");
+        assertEquals("newTournament", quiz.getTournament());
+        quiz.setTournament("tournament");
+    }
+
+    @Test
+    public void constructor() {
+        Quiz quiz = new Quiz("artName", null, "tournament");
+        assertEquals("artName", quiz.getArtName());
+        assertEquals("tournament", quiz.getTournament());
+    }
+
+
+
+
+
+}
