05cb84ba3118abd6bafb9e22dc7f2614641d0c77
lucamouchel <luca.mouchel@epfl.ch>
2023-05-22 13:58:20 +0200

    edit @before in tests

--- app/src/androidTest/java/ch/epfl/culturequest/ui/ProfileFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/culturequest/ui/ProfileFragmentTest.java
@@ -29,12 +29,17 @@ import org.junit.runner.RunWith;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import ch.epfl.culturequest.R;
 import ch.epfl.culturequest.authentication.Authenticator;
 import ch.epfl.culturequest.database.Database;
 import ch.epfl.culturequest.social.Post;
 import ch.epfl.culturequest.social.Profile;
+import ch.epfl.culturequest.storage.FireStorage;
 import ch.epfl.culturequest.ui.profile.ProfileFragment;
 import ch.epfl.culturequest.utils.ProfileUtils;
 
@@ -48,7 +53,7 @@ public class ProfileFragmentTest {
 
 
     @Before
-    public void setUp() throws InterruptedException {
+    public void setUp() throws InterruptedException, ExecutionException, TimeoutException {
         // Set up the database to run on the local emulator of Firebase
         Database.setEmulatorOn();
 
@@ -57,6 +62,9 @@ public class ProfileFragmentTest {
 
         //Set up the authentication to run on the local emulator of Firebase
         Authenticator.setEmulatorOn();
+        FireStorage.setEmulatorOn();
+        FireStorage.clearStorage();
+
 
         // Signs up a test user used in all the tests
         Authenticator.manualSignUp(email, password).join();
@@ -65,12 +73,15 @@ public class ProfileFragmentTest {
         Authenticator.manualSignIn(email, password).join();
 
         // picpath is the url to the profile pic of the test user
-        String picpath = "https://firebasestorage.googleapis.com/v0/b/culturequest.appspot.com/o/profilePictures%2FcT93LtGk2dT9Jvg46pOpbBP69Kx1?alt=media&token=35ba6af5-104d-4218-bc26-3fb39f75ac15";
-        Post post = new Post("abc", Authenticator.getCurrentUser().getUid(), picpath
-                , "Piece of Art", 0, 0, new ArrayList<>());
-        Database.uploadPost(post);
+
+        String downloadUrl = FireStorage.uploadAndGetUrlFromImage(FireStorage.getBitmapFromURL(DEFAULT_PROFILE_PIC_PATH), true)
+                .join();
 
         ProfileUtils.POSTS_ADDED = 0;
+
+        Post post = new Post("abc", Authenticator.getCurrentUser().getUid(), downloadUrl
+                , "Piece of Art", 0, 0, new ArrayList<>());
+        Database.uploadPost(post);
 
         profile = new Profile(Authenticator.getCurrentUser().getUid(), "Johnny Doe", "Xx_john_xX", "john.doe@gmail.com", "0707070707", DEFAULT_PROFILE_PIC_PATH, 35,new HashMap<>(), new ArrayList<>());
         Profile.setActiveProfile(profile);
