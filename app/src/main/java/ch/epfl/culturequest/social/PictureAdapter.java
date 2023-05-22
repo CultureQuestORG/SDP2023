509aa6f76fdbe1a8b02a625468efb2b06d309ab7
A <albert.troussard@epfl.ch>
2023-05-22 16:04:48 +0200

    improve robustness

--- app/src/main/java/ch/epfl/culturequest/social/PictureAdapter.java
+++ app/src/main/java/ch/epfl/culturequest/social/PictureAdapter.java
@@ -99,7 +99,7 @@ public class PictureAdapter extends RecyclerView.Adapter<PictureAdapter.PictureV
             holder.year.setText(artwork.getYear());
             holder.description.setText(shortenDescription(artwork.getSummary()));
             holder.score.setText("+" + artwork.getScore() + " pts");
-            holder.location.setText(artwork.getCity());
+            holder.location.setText(artwork.getCity()!=null ? artwork.getCity() : artwork.getCountry()!=null ? artwork.getCountry() : "World");
 
 
             // Put a see more button if the description is too long
