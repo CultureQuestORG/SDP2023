4e011362d9cd62193e3186135109f18a4c3b3751
Ahmed Elalamy <91589855+aelalamy42@users.noreply.github.com>
2023-05-25 10:08:57 +0200

    Update MapsFragment.java

--- app/src/main/java/ch/epfl/culturequest/ui/map/MapsFragment.java
+++ app/src/main/java/ch/epfl/culturequest/ui/map/MapsFragment.java
@@ -309,7 +309,7 @@ public class MapsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         SupportMapFragment mapFragment =
-                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapsFragment);
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
             mapFragment.getMapAsync(callback);
         }
